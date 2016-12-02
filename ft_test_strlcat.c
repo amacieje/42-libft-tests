6907094a92b82cae19b9e5f8f1c8a45de0848a66
Anna MACIEJEWICZ <amacieje@e3r13p6.42.fr>
2016-12-02 12:31:49 +0100

    lets test your ft_strlcat properly

--- ft_test_strlcat.c
+++ ft_test_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: amacieje <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 19:57:03 by amacieje          #+#    #+#             */
-/*   Updated: 2016/12/02 12:27:11 by amacieje         ###   ########.fr       */
+/*   Updated: 2016/12/02 12:31:44 by amacieje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,28 @@ void	*ft_test_strlcat(void)
 	int		i;
 
 	i = 0;
+	/*For your ft_strlcat*/
 	ft_putstr("ft_strlcat return value : ");
-	ft_putnbr(ft_strlcat(dst, src, 0));
+	ft_putnbr(ft_strlcat(dst, src, 0)); //set size value here
 	ft_putstr("\ndst after ft_strlcat : ");
 	ft_putstr(dst);
 	ft_putstr("\ndst casted in int : ");
 	while (i <= ft_strlen(dst))
 	{
-		ft_putnbr(dst[i]);
+		ft_putnbr(dst[i]); //check the presence of \0
 		ft_putstr(" ");
 		i++;
 	}
+	/*For the libc strlcat*/
 	ft_putstr("\nstrlcat return value : ");
-	ft_putnbr(strlcat(dst2, src2, 0));
+	ft_putnbr(strlcat(dst2, src2, 0)); //set size value here
 	ft_putstr("\ndst2 after strlcat : ");
 	ft_putstr(dst2);
 	ft_putstr("\ndst2 casted in int : ");
 	i = 0;
 	while (i <= ft_strlen(dst2))
 	{
-		ft_putnbr(dst2[i]);
+		ft_putnbr(dst2[i]); //then compare to your ft_strlcat
 		ft_putstr(" ");
 		i++;
 	}
